7c39b8fa64179aac463bcdf07f895a0f9c6eb101
Joao Pedro de Faria Ferreira <joao.pedro.ferreira.profissional@gmail.com>
2026-02-03 20:14:36 -0300

    first commit

---
+++ src/test/java/joao/faria/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package joao.faria.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
